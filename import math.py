abf6c2417699731948666eb1772568d8543fe266
Alana Prado <74723979+alanazip@users.noreply.github.com>
2024-12-02 11:25:09 -0300

    Update import math.py

--- import math.py
+++ import math.py
@@ -5,3 +5,5 @@ b = 19
 mmc = math.lcm(a, b)
 
 print(f"O MMC de {a} e {b} é {mmc}.")
+
+# esse é um código do primeiro dia do desafio (advent of code) sobre: descobrir o MMC de 2 numeros aleatorios usando a linguagem de programação de sua preferencia.

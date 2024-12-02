5c3d9d02fc803ea178f32553bf5c8e427e228316
Alana de Souza <alananascimentodesouza@gmail.com>
2024-12-02 11:35:01 -0300

    day 2

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz ():
+    for numero in range (0, 100):
+        if (numero %3==0):
+            print('fizz')
+        elif (numero %5==0):
+            print('buzz')
+        else:
+            print(numero)
+fizzbuzz()
+
+# esse é um código do segundo dia do desafio (advent of code) sobre: fizzbuzz - um jogo de palavras que qualquer numero divisivel por tres traz a palavra fizz, se divisivel por 5 a palavra buzz e se nao for divisivel só mostra o numero. usando a linguagem de programação de sua preferencia: Python

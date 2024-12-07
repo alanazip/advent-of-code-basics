11d66447382b5f27d2d66e437ded4f118ee87b38
Alana Prado <74723979+alanazip@users.noreply.github.com>
2024-12-07 15:14:06 -0300

    day 7

---
+++ lista.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+    def delete(self, key):
+        current = self.head
+
+        # If head node itself holds the key
+        if current and current.data == key:
+            self.head = current.next
+            current = None
+            return
+
+        # Search for the key to be deleted
+        prev = None
+        while current and current.data != key:
+            prev = current
+            current = current.next
+
+        # If the key is not present
+        if not current:
+            return
+
+        # Unlink the node from the linked list
+        prev.next = current.next
+        current = None
